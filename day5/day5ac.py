84cb7f54bb96a42670b6ec0a810f3ef70b2899c1
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2023-12-05 22:44:26 -0800

    add a community solution w/ correct answer for day 5 part1

---
+++ day5/day5ac.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# community solution -- far more straightforward than what I attempted
+with open("input.txt", "r") as file:
+    data = file.readlines()
+
+seeds = [int(i) for i in data[0].strip().split(": ")[1].split(" ")]
+mappings = []
+for line in data[2:]:
+    line = line.strip()
+    if line.endswith(":"):
+        mappings.append([])
+    elif len(line) > 0:
+        mappings[-1].append([int(i) for i in line.split(" ")])
+
+[m.sort(key=lambda x: x[1]) for m in mappings]
+
+res = 2**32
+for x in seeds:
+    for typemappings in mappings:
+        for mapping in typemappings:
+            if x >= mapping[1] and x < mapping[1] + mapping[2]:
+                x = x - mapping[1] + mapping[0]
+                break
+    res = min(x, res)
+print(res)
+
+# produces correct answer for my input: 346433842:
