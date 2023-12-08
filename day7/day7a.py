116a6830041aa9d5b34d149d73fdbe33baf32ad7
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2023-12-08 11:45:51 -0800

    add method to bucket hands by type; use naive iterative checks w/ control flow

--- day7/day7a.py
+++ day7/day7a.py
@@ -20,4 +20,48 @@ with open('input.txt') as file:
         hands.append((hand, int(bid)))
 
 def bucketHand(handInst):
-    hand = ''.join(handInst[0].sorted())
+    counts = dict()
+    for c in handInst[0]:
+        if counts.get(c, None):
+            counts[c] += 1
+        else:
+            counts[c] = 1
+
+    havePair = False
+    have3ofKind = False
+    have2Pair = False
+    for _, v in counts.items():
+        if v == 5:
+            handBuckets['fiveOfKind'].append(handInst)
+            return
+        if v == 4:
+            handBuckets['fourOfKind'].append(handInst)
+            return
+        if v == 3:
+            have3ofKind = True
+            continue
+        if v == 2:
+            if havePair:
+                have2Pair = True
+            else:
+                havePair = True
+            continue
+    if have3ofKind and havePair:
+        handBuckets['fullHouse'].append(handInst)
+        return
+    if have3ofKind:
+        handBuckets['threeOfKind'].append(handInst)
+        return
+    if have2Pair:
+        handBuckets['twoPair'].append(handInst)
+        return
+    if havePair:
+        handBuckets['onePair'].append(handInst)
+        return
+    handBuckets['highCard'].append(handInst)
+
+for h in hands:
+    bucketHand(h)
+
+for k,v in handBuckets.items():
+    print(f'handType: {k}, hands of type: {v}')
