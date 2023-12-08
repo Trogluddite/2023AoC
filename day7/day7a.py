5edb5dfe8f95795c658ec18787ffa6481d6bbade
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2023-12-07 23:23:05 -0800

    day7a WIP

--- day7/day7a.py
+++ day7/day7a.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+
+hands = list(tuple())
+handBuckets = {
+        "fiveOfKind"    : list(),
+        "fourOfKind"    : list(),
+        "fullHouse"     : list(),
+        "threeOfKind"   : list(),
+        "twoPair"       : list(),
+        "onePair"       : list(),
+        "highCard"      : list(),
+    }
+
 with open('input.txt') as file:
     data = file.readlines()
+    for l in data:
+        if len(l.strip()) == 0:
+            continue
+        hand, bid = l.split(' ')
+        hands.append((hand, int(bid)))
 
+def bucketHand(handInst):
+    hand = ''.join(handInst[0].sorted())
