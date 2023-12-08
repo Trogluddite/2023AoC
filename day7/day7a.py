49a89478374123ebcca2fb1a7cdc6231c0db9c93
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2023-12-08 12:42:22 -0800

    mostly working solition for part 1; bug noted

--- day7/day7a.py
+++ day7/day7a.py
@@ -62,11 +62,29 @@ def bucketHand(handInst):
 
 for h in hands:
     bucketHand(h)
-
-
-cardRanks = dict(zip( list('AKQJT98765432'), range(1,14)))
-
+cardRanks = dict(zip( list('23456789TJQKA'), range(1,14)))
 for k,v in handBuckets.items():
     # sort buckets by first card in hand, ranked by cardRank
-    handBuckets[k] = sorted(handBuckets[k], key=(lambda h: cardRanks[ h[0][0] ]) )
-print(handBuckets)
+    # higher card gets higher rank #
+    # FIXME: bug here; if first card matches, it should sort based on second card
+    handBuckets[k] =  sorted(handBuckets[k], key=(lambda h: cardRanks[ h[0][0] ]))
+
+# generator to rank all hands, where higher rank is later in list
+# and thus corresponds to a higher enumerated value
+keyOrder = ['fiveOfKind', 'fourOfKind', 'fullHouse', 'threeOfKind', 'twoPair', 'onePair', 'highCard']
+keyOrder.reverse()
+def rankedHands():
+    for k in keyOrder:
+        for x in iter( handBuckets[k] ):
+            yield x
+
+handVals = list()
+for k, h in enumerate(rankedHands()):
+    print(f'rank: {(k+1)}, hand: {h}')
+for r, h in enumerate(rankedHands()):
+    # r+1 is hand ranking
+    # h[1] is hand bid
+    handVals.append( (r+1) * (h[1]) )
+
+print(handVals)
+print(sum(handVals))
