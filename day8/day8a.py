376a4e3904a5342fca68966e20d60c0bda3eda02
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2023-12-09 00:00:09 -0800

    part1 solution

--- day8/day8a.py
+++ day8/day8a.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python3
 
 import sys
+from itertools import cycle
 
 if len(sys.argv) > 1:
     filename = sys.argv[1]
 else:
     filename = "input.txt"
 
-inLines = list()
-
+directions = ''
+mappings = dict()
 with open(filename) as file:
-    data = file.readlines()
-    for l in data:
+    dataIter = iter(file.readlines())
+    directions = next(dataIter)
+    directions = directions.strip()
+    for l in dataIter:
         if len(l.strip()) == 0:
             continue
-        inLines.append(l)
+        k, m = l.split('=')
+        k = k.strip()
+        mLft, mRgt = m.split(',')
+        mLft = mLft.strip('() \n')
+        mRgt = mRgt.strip('() \n')
+        mappings[k] = dict()
+        mappings[k]['L'] = mLft
+        mappings[k]['R'] = mRgt
+
+def walkAndCount():
+    stepCount = 0
+    currKey = 'AAA'
+    dirIter = cycle(directions)
+    while currKey != 'ZZZ':
+        currKey = mappings[currKey][next(dirIter)]
+        stepCount += 1
+    return stepCount
+print(walkAndCount())
+
+# produces correct answer for part 1: 19241
+
