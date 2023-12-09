b7d547fe5ea7d990e7c68a4c5cf4c4c6bd64265f
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2023-12-09 00:06:02 -0800

    start day 8 part2

---
+++ day8/day8b.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+from itertools import cycle
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    filename = "input.txt"
+
+directions = ''
+mappings = dict()
+with open(filename) as file:
+    dataIter = iter(file.readlines())
+    directions = next(dataIter)
+    directions = directions.strip()
+    for l in dataIter:
+        if len(l.strip()) == 0:
+            continue
+        k, m = l.split('=')
+        k = k.strip()
+        mLft, mRgt = m.split(',')
+        mLft = mLft.strip('() \n')
+        mRgt = mRgt.strip('() \n')
+        mappings[k] = dict()
+        mappings[k]['L'] = mLft
+        mappings[k]['R'] = mRgt
+
+def walkAndCount():
+    stepCount = 0
+    dirIter = cycle(directions)
+
+print(walkAndCount())
+
+
